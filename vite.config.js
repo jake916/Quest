7954498be4f92aa13e6abf7da30f99fa37cbccad
jake916 <jakeayodeji@gmail.com>
2025-04-16 14:36:34 +0100

    allowed host update for render

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Bind to all network interfaces
     port: process.env.PORT || 5173, // Use the PORT environment variable or default to 5173
+    allowedHosts: ['quest-3ica.onrender.com'], // Allow requests from this host
   },
 })
