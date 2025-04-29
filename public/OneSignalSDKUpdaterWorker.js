9da4982fa74f22e16e72b83ee35998ece3a7e977
jake916 <jakeayodeji@gmail.com>
2025-04-29 13:58:56 +0100

    notification update

---
+++ public/OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,4 @@
+// This is the OneSignal updater service worker file required for push notifications.
+// You can customize this file if needed, but the default content is sufficient for most cases.
+
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKUpdaterWorker.js');
